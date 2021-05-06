4da89e9056c1aeed89e4d725c665cb36fb5a7b76
y-yanagii <yyanagisawa555@gmail.com>
2021-05-06 11:07:41 +0900

    ゲーム画面の土台作成（#106）

---
+++ components/pages/games/game.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      selectedMode: this.$store.getters['localStorages/choiceMode'],
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
