d0c74792a5c4117ca4818e309606a836e27dde0b
y-yanagii <yyanagisawa555@gmail.com>
2021-05-11 02:19:25 +0900

    パズルゲームのスタート画面作成（#114）

--- components/Start.vue
+++ components/Start.vue
@@ -88,6 +88,12 @@ export default {
           modeTypeTitleClass: 'title-logo-bubble',
           modeTypeButtonClass: 'start-button-bubble',
         }
+      } else if (modeType === this.$mode.puzzle) {
+        // PUZZLEの場合、黄緑
+        return {
+          modeTypeTitleClass: 'title-logo-puzzle',
+          modeTypeButtonClass: 'start-button-puzzle',
+        }
       } else {
         // 難易度EASY, NORMAL, HARDの場合、デフォルト色
         return {
@@ -120,5 +126,8 @@ export default {
 }
 .title-logo-bubble {
   color: $bubble-color !important;
+}
+.title-logo-puzzle {
+  color: $puzzle-color !important;
 }
 </style>
