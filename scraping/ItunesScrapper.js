a994d7128d090733cd222ae595ddfdba3217832e
y-yanagii <yyanagisawa555@gmail.com>
2021-05-17 01:35:12 +0900

    テストデータをJSON形式のファイルに出力（#96）

--- scraping/ItunesScrapper.js
+++ scraping/ItunesScrapper.js
@@ -1,6 +1,8 @@
 // PuppeteerによるWebスクレイピング
 const puppeteer = require('puppeteer');
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const fs = require('fs');
+const firebaseAdmin = require('firebase-admin');
 
 let tests = []; // テストコレクションオブジェクト
 // テストドキュメント
@@ -53,31 +55,32 @@ let songs = []; // 曲名のみの配列も保持
 
 // iTuensAPIより楽曲情報の取得
 let request = new XMLHttpRequest(); // HTTP通信用のオブジェクト
-request.open('GET', "https://itunes.apple.com/search/?term=BLACKPINK&entity=musicTrack", false); // リクエスト設定
+request.open('GET', "https://itunes.apple.com/search/?term=BLACKPINK&entity=musicTrack&limit=100", false); // リクエスト設定
 request.send(); // リクエスト投げる
 let iTuensApiDatas = JSON.parse(request.responseText); // 返ってきたレスポンスがstringのためJSON.parse
+console.log(iTuensApiDatas.results.length)
 
 // ここからスクレイピング処理（ブラウザ起動）
 puppeteer.launch({
   headless: false, // ヘッドレスをオフ（ブラウザが見えるように）
-  slowMo: 300      // 何が起こっているかを分かりやすくするため動作遅延
+  slowMo: 100      // 何が起こっているかを分かりやすくするため動作遅延
 }).then(async browser => {
   const page = await browser.newPage() // ブラウザの新しいタブを表示
   await page.goto('https://music.apple.com/jp/search/song?term=blackpink'); // iTunesのURLにアクセス（曲検索でブラックピンクを指定して検索した画面にアクセス）
   await page.waitForSelector('.context-menu__overflow'); // 指定した要素が表示されるまで待つ
-  const menus = await page.$$(".songs-list .songs-list-row"); // 曲の行リストdiv
-
+  await page.waitForSelector('.songs-list-row'); // 指定した要素が表示されるまで待つ
   await page.evaluate(() => { document.getElementsByClassName('songs-list-row')[24].scrollIntoView(true); }); // 曲リストを最下部までスクロールさせ曲リストを取得してくる
   await page.waitForSelector('div .songs-list-row:nth-child(30)') // 曲リストが増えるのを待つ
   await page.evaluate(() => { document.getElementsByClassName('songs-list-row')[74].scrollIntoView(true); }); // 再度曲リストを追加し50曲以上にする（iTuensAPIで取得した試聴データが50件のため）
   await page.waitForSelector('div .songs-list-row:nth-child(80)') // 曲リストが増えるのを待つ
-console.log(menus.length)
+  const menus = await page.$$(".songs-list .songs-list-row"); // 曲の行リストdiv
+console.log(menus.length);
   // 曲の行をループ
   for (const menu of menus) {
     let siteData = { songName: "", embed: "" }; // 曲の行データ（曲名、embedコード）
-    const menuDiv = await menu.$('div.dt-media-contextual-menu.songs-list-row__context-menu'); // 「•••」のdiv部分
-    if (menuDiv !== null) {
+    if (menu !== null) {
       await menu.click(); // 曲の行をクリック（フォーカスさせたいため）
+      const menuDiv = await menu.$('.songs-list-row__context-menu'); // 「•••」のdiv部分
       await menuDiv.click(); // 「•••」をクリックし共有liを表示
       const menuShare = await page.$('.context-menu__option.context-menu__option--parent'); // 共有liをクリックした時のサブメニュー
       await menuShare.hover(); // 共有liにフォーカス
@@ -106,7 +109,7 @@ console.log(menus.length)
   let arrayNums = [0, 1, 2, 3];
 
   // 試聴データ分ループ
-  for (const iTuensApiData of iTuensApiDatas) {
+  for (const iTuensApiData of iTuensApiDatas.results) {
     const matchData = ituensSiteDatas.find(itunesSite => itunesSite.songName === iTuensApiData.trackName); // 全く同じ曲名を取得
 
     // 取得できた場合
@@ -116,7 +119,7 @@ console.log(menus.length)
       // 選択値の要素数をランダムで定義
       arrayNums = arrayShuffle(arrayNums);
       // 正答の曲名以外の曲名リストを作成し、シャッフル後の先頭から3つを取得（正答以外の選択値）
-      let refinedSongNames = songs.filter(s => s !== answerSongName);
+      let refinedSongNames = arrayShuffle(songs.filter(s => s !== answerSongName));
       refinedSongNames = refinedSongNames.slice(0, 3); // 正答以外の選択値
       refinedSongNames.push(answerSongName); // 正答の曲名も追加
       refinedSongNames = arrayShuffle(refinedSongNames); // 選択値の曲名配列をシャッフル
@@ -139,6 +142,76 @@ console.log(menus.length)
       tests.push(test);
     }
   }
+  console.log(tests.length);
+
+  // jsonファイル出力（fsはコールバック関数出なくてはならないためcreateFile）
+  const source = { tests: tests };
+  const createFile = (pathName, source) => {
+    const toJSON = JSON.stringify(source);
+    fs.writeFile(pathName, toJSON, (err) => {
+      if (err) rej(err);
+    });
+  };
+
+  createFile('scraping/newTests.json', source); // JSONファイル生成
+  let rawData = fs.readFileSync('scraping/newTests.json'); // 読み込み用Jsonファイルデータ
+  let registerTests = JSON.parse(rawData); // JSONファイルをJSONデータへ変換
+  // Firebase設定
+  firebaseAdmin.initializeApp({
+    apiKey: "AIzaSyDchpCeBzZM6r1sgqxaLPcj3MZV9gEjqcM",
+    authDomain: "blackpink-test.firebaseapp.com",
+    projectId: "blackpink-test"
+  });
+
+  const db = firebaseAdmin.firestore();
+
+  registerTests.forEach(function(obj) {
+    db.collection("tests").doc().set({
+      embedInfo: {
+        detail: {
+          subCode: obj.embedInfo.detail.subCode,
+          subContent: obj.embedInfo.detail.subContent,
+        },
+        embedCode: obj.embedInfo.embedCode,
+        embedType: obj.embedInfo.embedType,
+      },
+      modeType: obj.modeType,
+      options: [
+        {
+          answer: {
+            isAnswer: obj.options[0].answer.isAnswer,
+            isNumber: obj.options[0].answer.isNumber,
+          },
+          optionContent: obj.options[0].optionContent,
+        },
+        {
+          answer: {
+            isAnswer: obj.options[1].answer.isAnswer,
+            isNumber: obj.options[1].answer.isNumber,
+          },
+          optionContent: obj.options[1].optionContent,
+        },
+        {
+          answer: {
+            isAnswer: obj.options[2].answer.isAnswer,
+            isNumber: obj.options[2].answer.isNumber,
+          },
+          optionContent: obj.options[2].optionContent,
+        },
+        {
+          answer: {
+            isAnswer: obj.options[3].answer.isAnswer,
+            isNumber: obj.options[3].answer.isNumber,
+          },
+          optionContent: obj.options[3].optionContent,
+        },
+      ],
+      question: obj.question,
+      questionType: obj.questionType,
+    }).then(function(docRef) {
+      console.log("ID: ", docRef.id);
+    });
+  });
 
 });
 
