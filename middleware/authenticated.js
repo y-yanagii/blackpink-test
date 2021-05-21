28c58a0a2cd976ff8814c506dfe6fec999ea869b
y-yanagii <yyanagisawa555@gmail.com>
2021-05-22 02:44:36 +0900

    アカウント表示時リロードすると、ミドルウェアの非同期が終わらなくてユーザ情報取得できない（#137）

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,11 +1,11 @@
 import firebase from '~/plugins/firebase';
 
-export default function(context) {
+export default async function(context) {
   console.log("authenticated");
   const guest = context.store.getters["localStorages/getGuestPlay"];
   if (guest) return; // ゲストの場合認証チェックスキップ
   // Twitter認証済みチェック
-  firebase.auth().onAuthStateChanged((user) => {
+  await firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // ユーザ情報をstoreに設定
       context.store.dispatch("twitter/setUser", user);
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>{{ user }}</div>
+  <div>
+    <div>
+      {{ user }}
+    </div>
+    <div>
+      {{ getUser }}
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,14 +22,23 @@ export default {
       }
     }
   },
+  computed: {
+    getUser() {
+      console.log("getUser");
+    }
+  },
   mounted() {
     let currentUser = this.$store.getters['twitter/user'];
     this.user.uid = currentUser.uid;
     this.user.name = currentUser.name;
     this.user.isLogin = currentUser.isLogin;
     this.user.photoURL = currentUser.uid;
-    this.user.privacy = this.$store.getters['statuses/getStatus', currentUser.uid].privacy
-  }
+    debugger
+    this.user.privacy = this.$store.getters['statuses/getStatus', currentUser.uid]
+  },
+  created() {
+    this.$store.dispatch('statuses/init');
+  },
 }
 </script>
 
