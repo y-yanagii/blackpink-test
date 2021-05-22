b0e047fc5f4adc6bd5928e7027976e492b98a112
y-yanagii <yyanagisawa555@gmail.com>
2021-05-22 15:37:17 +0900

    プロフィール画面のレイアウト途中（#137）

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,11 +1,43 @@
 <template>
   <div>
-    <div>
-      {{ user }}
-    </div>
-    <div>
-      {{ getUser }}
-    </div>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="6">
+        <div class="text-center">
+          <div class="text-center">
+            <div class="OtherLogo">
+              <span class="title-logo">
+                PROFILE
+              </span>
+            </div>
+          </div>
+          <!-- プロフィール領域 -->
+          <div class="text-center">
+            <div class="profile-template">
+              <div class="avater-name">
+                <div class="avatar">
+                  <v-avatar
+                    size="56"
+                  >
+                    <v-img
+                      :lazy-src="user.photoURL"
+                      max-height="150"
+                      max-width="250"
+                      :src="user.photoURL"
+                    ></v-img>
+                  </v-avatar>
+                </div>
+                <div>
+                  <div>{{ user.name }}</div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- ランク領域 -->
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -32,8 +64,7 @@ export default {
     this.user.uid = currentUser.uid;
     this.user.name = currentUser.name;
     this.user.isLogin = currentUser.isLogin;
-    this.user.photoURL = currentUser.uid;
-    debugger
+    this.user.photoURL = currentUser.photoURL;
     this.user.privacy = this.$store.getters['statuses/getStatus', currentUser.uid]
   },
   created() {
@@ -43,5 +74,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.profile-template {
+  padding: 3%;
+  width: 100%;
+  border: solid 2px $base-text-color;
+  border-radius: 6px;
+  .avater-name {
+    padding: 3%;
+    display: flex;
+    .avatar {
+      margin-right: 12px;
+    }
+  }
+}
 </style>
