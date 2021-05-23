a85600841eac4b19c1d3d2927eedd445de365f9f
y-yanagii <yyanagisawa555@gmail.com>
2021-05-23 18:56:57 +0900

    メニューにブリンクス追加（#141）

---
+++ pages/blinks/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col cols="12" md="6">
+        <div class="text-center">
+          <div class="text-center">
+            <div class="OtherLogo">
+              <span class="title-logo">
+                BLINKS
+              </span>
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  created() {
+    this.$store.dispatch('users/init');
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
