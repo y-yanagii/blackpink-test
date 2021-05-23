49a900817bab72a3ba1d04532ca130de5331a2fc
y-yanagii <yyanagisawa555@gmail.com>
2021-05-23 20:02:58 +0900

    ユーザ一覧の雛形作成（#141）

--- pages/blinks/index.vue
+++ pages/blinks/index.vue
@@ -10,6 +10,29 @@
               </span>
             </div>
           </div>
+          <div class="text-center">
+            <div class="blinks">
+              <div
+                v-for="user in users"
+                :key="user.id"
+                class="blink"
+              >
+                <div>
+                  <v-avatar
+                    size="56"
+                  >
+                    <v-img
+                      :lazy-src="user.photoURL"
+                      max-height="150"
+                      max-width="250"
+                      :src="user.photoURL"
+                    ></v-img>
+                  </v-avatar>
+                </div>
+                <div>{{ user.name }}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </v-col>
     </v-row>
@@ -17,8 +40,21 @@
 </template>
 
 <script>
+import { db } from '~/plugins/firebase.js';
 
 export default {
+  data: function() {
+    return {
+      users: [],
+    }
+  },
+  firestore: {
+    // publicなユーザを全件取得
+    users: db.collection("users").where('privacy', '==', false),
+  },
+  methods: {
+
+  },
   created() {
     this.$store.dispatch('users/init');
   }
