35b1ba9a0344a3a27a10eff2b86847440ba95e6e
y-yanagii <yyanagisawa555@gmail.com>
2021-05-27 04:00:47 +0900

    1on1のキャンセルボタン実装（#21）

--- components/battles/Battle.vue
+++ components/battles/Battle.vue
@@ -3,6 +3,10 @@
     <v-row justify="center" align="center">
       <v-col cols="12" sm="8">
         <ProgressLinear v-if="isDisplayNum === 0"></ProgressLinear>
+        <CancelBack
+          v-if="isDisplayNum === 0"
+          :cancelVal="cancelVal"
+        ></CancelBack>
       </v-col>
     </v-row>
   </div>
@@ -10,19 +14,21 @@
 
 <script>
 import ProgressLinear from '~/components/ui/ProgressLinear.vue';
+import CancelBack from '~/components/ui/CancelBack.vue';
 
 export default {
   data: function() {
     return {
       isDisplayNum: 0,
+      cancelVal: this.$mode.oneonone,
     }
   },
   components: {
     ProgressLinear,
+    CancelBack,
   }
 }
 </script>
 
 <style scoped lang="scss">
-
 </style>
---
+++ components/ui/CancelBack.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="cancel-back-area">
+    <v-btn
+      @click="cancel()"
+      class="cancel-back-btn"
+    >
+      C &nbsp;&nbsp;&nbsp; A &nbsp;&nbsp;&nbsp; N &nbsp;&nbsp;&nbsp; C &nbsp;&nbsp;&nbsp; E &nbsp;&nbsp;&nbsp; L
+    </v-btn>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ["cancelVal"],
+  methods: {
+    cancel() {
+      this.$router.push({ path: "/mode" });
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.cancel-back-area {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  margin: 0 auto 20px auto;
+  width: 100%;
+  text-align: center;
+  .cancel-back-btn {
+    margin: auto;
+    width: 65%;
+    background: none;
+    color: $base-text-color;
+  }
+}
+</style>
