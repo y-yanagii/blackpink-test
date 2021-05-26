8adcad4b327a07bf61bc72a08a022f566a017267
y-yanagii <yyanagisawa555@gmail.com>
2021-05-27 02:24:41 +0900

    1on1の対戦相手待ち状態のバー実装（#21）

--- components/battles/Battle.vue
+++ components/battles/Battle.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    aaa
+    <v-row justify="center" align="center">
+      <ProgressLinear></ProgressLinear>
+    </v-row>
   </div>
 </template>
 
 <script>
+import ProgressLinear from '~/components/ui/ProgressLinear.vue';
+
 export default {
+  components: {
+    ProgressLinear,
+  }
 }
 </script>
 
---
+++ components/ui/ProgressLinear.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wait-bar">
+    <v-progress-linear
+      color="#f4a6b8"
+      indeterminate
+      rounded
+      height="6"
+    ></v-progress-linear>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped lang="scss">
+.wait-bar {
+
+}
+</style>
