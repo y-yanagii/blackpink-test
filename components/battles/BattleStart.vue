c11e33bbcf9a1350be2e430c406b186cb3482171
y-yanagii <yyanagisawa555@gmail.com>
2021-05-30 17:02:36 +0900

    1対1のスタート画面作成（#21）

--- components/battles/BattleStart.vue
+++ components/battles/BattleStart.vue
@@ -1,18 +1,68 @@
 <template>
-  <div>
-    
+  <div class="text-center battle-start">
+    <div class="user user-1">
+      <transition
+        name="user"
+        enter-active-class="animated slideInLeft"
+        appear
+      >
+        <div :class="this.$vuetify.breakpoint.xs ? 'font-size-xs' : ''">{{userNames[0]}}</div>
+      </transition>
+    </div>
+    <div class="vs" :class="this.$vuetify.breakpoint.xs ? 'font-size-xs-vs' : ''">VS</div>
+    <div class="user user-2">
+      <transition
+        name="user"
+        enter-active-class="animated slideInRight"
+        appear
+      >
+        <div :class="this.$vuetify.breakpoint.xs ? 'font-size-xs' : ''">{{userNames[1]}}</div>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   mounted() {
-
+    // 3秒間表示
+    setTimeout(() => {
+      // 表示制御
+      this.$emit('display-control');
+    }, 3000)
   },
-  props: ["isDisplayNum"],
+  props: ["isDisplayNum", "userNames"],
 }
 </script>
 
 <style scoped lang="scss">
-
+.battle-start {
+  margin: 100px 0px;
+  width: 100%;
+  .user {
+    margin: 10% auto;
+    text-align: center;
+    width: 60%;
+    max-width: 60%;
+    border-bottom: solid 2px $base-text-color;
+    div {
+      font-size: 30px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .font-size-xs {
+      font-size: 17px !important;
+    }
+  }
+  .vs {
+    margin: 10%;
+    font-style: italic;
+    font-size: 30px;
+    color: $base-text-color;
+  }
+  .font-size-xs-vs {
+    font-size: 17px !important;
+  }
+}
 </style>
--- pages/rooms/_id.vue
+++ pages/rooms/_id.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <v-row justify="center" align="center">
-      <v-col cols="12" sm="8">
-        
+      <v-col cols="12" sm="12">
+        <transition
+          name="battleStart"
+          leave-active-class="animated fadeOut"
+          appear
+        >
+          <BattleStart
+            v-if="isDisplayNum === $battleDisplayNum.battle"
+            @display-control="displayControl"
+            :isDisplayNum="isDisplayNum"
+            :userNames="userNames"
+          ></BattleStart>
+        </transition>
       </v-col>
     </v-row>
-    <BattleStart
-      v-if="isDisplayNum === $battleDisplayNum.battle"
-      :isDisplayNum="isDisplayNum"
-    ></BattleStart>
   </div>
 </template>
 
@@ -19,6 +26,14 @@ export default {
   data: function() {
     return {
       isDisplayNum: 0,
+      userNames: ["柳澤_RUNTEQ17期生", "山田 太郎"]
+    }
+  },
+  methods: {
+    displayControl() {
+      if (this.isDisplayNum === this.$battleDisplayNum.battle) {
+        this.isDisplayNum = this.$battleDisplayNum.start;
+      }
     }
   },
   validate(context) {
