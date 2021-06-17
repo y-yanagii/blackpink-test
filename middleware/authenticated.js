ffa8efc8b578d84f13604938262dcecfda164c13
y-yanagii <yyanagisawa555@gmail.com>
2021-06-17 23:47:23 +0900

    マネジメント画面は柳澤のみ入れる（#202）

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -8,7 +8,7 @@ export default async function(context) {
       // プロフィール画面へは遷移しない
       context.store.dispatch('messages/setSnackbarText', "ログインしてください");
       context.redirect('/');
-    } else if (context.route.name === "management") {
+    } else if (context.route.name === process.env.MANAGEMENT_URL) {
       // ゲストモード時は問答無用でマネジメント画面に遷移させない
       context.next(false);
     }
@@ -17,6 +17,11 @@ export default async function(context) {
   }
   // Twitter認証済みチェック
   await firebase.auth().onAuthStateChanged((user) => {
+    if (context.route.name === process.env.MANAGEMENT_URL) {
+      // マネジメント画面に関しては管理者のみ
+      let currentUser = firebase.auth().currentUser;
+      if (currentUser.uid !== process.env.ADMIN_UID) context.next(false);
+    }
     if (!user) {
       // ユーザ認証失敗
       // ホーム画面にリダイレクト
