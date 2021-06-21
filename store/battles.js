fd0fceb3f6ae70a5dd19e80fdcda9084c0bb91a0
y-yanagii <yyanagisawa555@gmail.com>
2021-06-21 15:20:17 +0900

    battles登録時ユーザ情報のreferenceも同時に登録するよう修正(#211)

--- store/battles.js
+++ store/battles.js
@@ -16,28 +16,37 @@ const actions = {
   init: firestoreAction(({ bindFirestoreRef }) => {
     bindFirestoreRef('battles', battlesRef)
   }),
-  winUpdate: firestoreAction((context, payload) => {
+  winUpdate: firestoreAction(async (context, payload) => {
+    // ユーザ情報をreferenceとして保持
+    const userDocSnapshot = await firebase.auth().currentUser !== null ? db.collection('users').doc(firebase.auth().currentUser.uid) : "";
     const id = getCurrentUserId(payload.twitterId, firebase.auth().currentUser);
     // set merge trueでなければ登録あれば追加
     battlesRef.doc(id).set({
       name: payload.name,
       win: firebase.firestore.FieldValue.increment(1),
+      user: userDocSnapshot,
     }, { merge: true });
   }),
-  loseUpdate: firestoreAction((context, payload) => {
+  loseUpdate: firestoreAction(async (context, payload) => {
+    // ユーザ情報をreferenceとして保持
+    const userDocSnapshot = await firebase.auth().currentUser !== null ? db.collection('users').doc(firebase.auth().currentUser.uid) : "";
     const id = getCurrentUserId(payload.twitterId, firebase.auth().currentUser);
     // set merge trueでなければ登録あれば追加
     battlesRef.doc(id).update({
       name: payload.name,
       lose: firebase.firestore.FieldValue.increment(1),
+      user: userDocSnapshot,
     }, { merge: true });
   }),
-  drawUpdate: firestoreAction((context, payload) => {
+  drawUpdate: firestoreAction(async (context, payload) => {
+    // ユーザ情報をreferenceとして保持
+    const userDocSnapshot = await firebase.auth().currentUser !== null ? db.collection('users').doc(firebase.auth().currentUser.uid) : "";
     const id = getCurrentUserId(payload.twitterId, firebase.auth().currentUser);
     // set merge trueでなければ登録あれば追加
     battlesRef.doc(id).update({
       name: payload.name,
       draw: firebase.firestore.FieldValue.increment(1),
+      user: userDocSnapshot,
     });
   }, { merge: true }),
 }
@@ -51,6 +60,11 @@ export default {
   getters,
   mutations,
   actions,
+}
+
+// ユーザ情報取得
+function getUser() {
+
 }
 
 // 自分のユーザIDを取得
